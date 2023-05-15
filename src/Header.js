3da89c5ea53d8173668726c9eddce6b82a280a9e
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-15 10:23:25 -0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css'
+
+function Header(){
+    return (
+        <header className='header1'>
+            <h1>My Header</h1>
+            <ul className="list1">
+                <li>About</li>
+                <li>Buy</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    )
+}
+export default Header;
