a9e20304e8ebcc844eb67735594c0bb443aa1b81
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-18 10:16:47 -0300

    add Menu

--- src/Header.js
+++ src/Header.js
@@ -5,12 +5,19 @@ function Header(props){
     return (
         <header className='header1'>
             <h1>{props.name}</h1>
-            <ul className="list1">
+            <Menu links={props.links}></Menu>
+        </header>
+    )
+}
+
+function Menu(props){
+    return(
+        <ul className="list1">
                 <li>{props.links[0]}</li>
                 <li>{props.links[1]}</li>
                 <li>{props.links[2]}</li>
             </ul>
-        </header>
     )
 }
+
 export default Header;
