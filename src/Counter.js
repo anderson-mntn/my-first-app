9291c3c867a7ab9a30284fbc6d436a0a3a423ede
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-22 09:45:33 -0300

    Gerenciamento de Estados na função - useEffect

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,40 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 function Counter(props){
 
-    const [contador, setContador] = useState(props.count) // or useState(0)
-    const [meuEstado, setMeuEstado] = useState("alguma coisa")
+    const [count, setCount] = useState(props.count) // or useState(0) - Gerenciamento de estado do componente
+    
 
+    // Gerenciando estados
+
+    // useEffect é executado sempre que um estado for atualizado
+
+    // useEffect(() => {}) atualiza qualquer estado que for modificado e executa a função descrita dentro - componentDidUpdate() sem especificar estado (atualizar pra qualquer coisa)
+
+    // useEffect(() => {}, [nomeDoEstado]) atualiza apenas para o estado passado no array - componentDidUpdate() atualiza quando o estado especificado for alterado
+
+    // useEffect(() => {}, []) executa uma unica vez - componentDidMount()
+
+
+    useEffect(()=>{
+        setCount(parseInt(localStorage.getItem('count')))
+
+    },[])
+
+    useEffect(()=>{
+        document.title = count;
+        localStorage.setItem('count', count)
+    })
 
     function add(){
-        setContador(contador + 1);
-        console.log("add")
+        setCount(count + 1);
 
-        console.log(meuEstado)
     }
 
     return(
         <div>
-            <h1>Counter: {contador}</h1>
+            <h1>Counter: {count}</h1>
             <button onClick={add}>Add</button>
         </div>
     )
